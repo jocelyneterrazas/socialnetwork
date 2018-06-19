6e18125294037d46e7402730f66060565914d4f6
jocelyneterrazas <40184482+jocelyneterrazas@users.noreply.github.com>
2018-06-19 14:42:47 -0700

    Add files via upload

--- socialnetwork.py
+++ socialnetwork.py
@@ -3,54 +3,82 @@
 
 
 class User:
-    def __init__(self, firstName, lastName, username, bio, userID):
-        self.firstName = firstName
-        self.lastName = lastName
+    def __init__(self, username):
         self.username = username
-        self.bio = bio
-        self.userID = userID
-        self.friends = []
+        self.firstName = ""
+        self.lastName = ""
+        self.bio = ""
+        ##self.userID = userID
+        self.friendsList = []
         self.posts = []
+       
 
-    def addFriend(self, username):
-        self.friends.append(username)
-    ##def unFriend():
+    def addfirstName(self, firstName):
+        self.firstName = firstName
 
-    def viewNewsFeed (self, friends):
-       for friends in self.friends:
+    def addlastName(self, lastName):
+        self.lastName = lastName
+
+    def addbio(self, bio):
+        self.bio = bio
+
+    def addFriend(self, person):
+        self.friendsList.append(person)
+        
+    def addposts(self, posts):
+        self.posts.append(post)
+    
+        
+    def unFriend(self, obj):
+        for friend in self.friendsList:
+            if friend.username == obj.username:
+                self.friendsList.remove(obj)
+
+    def showUsernames(self):
+        for friend in self.friendsList:
+            print (friend.username)
+                
+    def viewNewsFeed(self):
+        for friends in self.friendsList:
            print (friends.posts)
            
         
 
 if __name__ == "__main__":
-    firstName = "Jocelyne"
-    lastName = "Terrazas"
-    username = "jocelyne"
-    bio = "hi"
-    userID = "0000"
 
-    jocelyne = User(firstName, lastName, username, bio, userID)
-    lucy = User("Lucy", "Lucille", "lucy", "hola", "0001")
-    churrosanchez = User("Churro", "Sanchez", "churrosanchez", "yeet", "0002")
-    thejazziestjaz = User("Jazmin", "Solis", "thejazziestjaz", "I like animals","0003")
+    username = "jocelyne"
+
+    jocelyne = User("jocelyne")
+    lucy = User("lucy")
+    churrosanchez = User("churrosanchez")
+    thejazziestjaz = User("thejazziestjaz")
 
     
-    print(jocelyne.firstName)
-    print(lucy.firstName)
-    print(churrosanchez.firstName)
-    print(thejazziestjaz.firstName)
+##    print(jocelyne.firstName)
+##    print(lucy.firstName)
+##    print(churrosanchez.firstName)
+##    print(thejazziestjaz.firstName)
 
-    jocelyne.addFriend("lucy")
-    jocelyne.addFriend("churrosanchez")
-    jocelyne.addFriend("thejazziestjaz")
-    print(jocelyne.friends)
+    jocelyne.addFriend(lucy)
+    jocelyne.addFriend(churrosanchez)
+    jocelyne.addFriend(thejazziestjaz)
+##    print(jocelyne.friendsList)
 
     lucy.posts.append("THIS IS MY FIRST POST")
-    print(lucy.posts)
-
     churrosanchez.posts.append("goodnight")
-    print(churrosanchez.posts)
-
     thejazziestjaz.posts.append("music can end war")
-    print(thejazziestjaz.posts)
-    jocelyne.viewNewsFeed(username)
+##    print(lucy.posts)
+##    print(churrosanchez.posts)
+##    print(thejazziestjaz.posts)
+print("This is your News Feed:")
+jocelyne.viewNewsFeed()
+
+print("This is your friends list:")  
+jocelyne.showUsernames()
+
+jocelyne.unFriend(churrosanchez)
+
+print("This is your UPDATED friends list:")
+jocelyne.showUsernames()
+
+
